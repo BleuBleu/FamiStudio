79e0f804844a47820586b0d4f8ba4199cbe410be
BleuBleu <bleubleu@outlook.com>
2019-10-06 11:09:17 +0800

    Fixing some mysterious corruption issues on flat buttons.

--- FamiStudio/UI/Dialogs/Gtk/FlatButton.cs
+++ FamiStudio/UI/Dialogs/Gtk/FlatButton.cs
@@ -9,56 +9,21 @@ namespace FamiStudio
     {
         bool focus;
         bool bold;
-        ImageSurface image;
-        static Pango.Context context;
-        Pango.Layout layoutNormal;
-        Pango.Layout layoutBold;
+        string buttonText;
+        Pixbuf pixbuf;
 
         public FlatButton(Pixbuf pb, string text = null)
         {
-            image = GdkPixbufToCairoImageSurface(pb);
+            pixbuf = pb;
+            buttonText = text;
 
             Events |= Gdk.EventMask.ButtonPressMask | EventMask.EnterNotifyMask | EventMask.LeaveNotifyMask | EventMask.ButtonReleaseMask;
 
-            if (text != null)
-            {
-                if (context == null)
-                {
-                    context = CreatePangoContext();
-                }
-
-                layoutNormal = new Pango.Layout(context);
-                layoutNormal.Alignment = Pango.Alignment.Left;
-                layoutNormal.SetText(text);
-                layoutNormal.FontDescription = Pango.FontDescription.FromString("Quicksand 14");
-
-                layoutBold = new Pango.Layout(context);
-                layoutBold.Alignment = Pango.Alignment.Left;
-                layoutBold.SetText(text);
-                layoutBold.FontDescription = Pango.FontDescription.FromString("Quicksand Bold 14");
-            }
-            else
+            if (text == null)
             {
                 WidthRequest  = 40;
                 HeightRequest = 40;
             }
-        }
-
-        unsafe ImageSurface GdkPixbufToCairoImageSurface(Pixbuf pb)
-        {
-            byte* p = (byte*)pb.Pixels.ToPointer();
-
-            for (int y = 0; y < pb.Height; y++, p += pb.Rowstride)
-            {
-                for (int x = 0; x < pb.Width * 4; x += 4)
-                {
-                    byte tmp = p[x + 0];
-                    p[x + 0] = p[x + 2];
-                    p[x + 2] = tmp;
-                }
-            }
-
-            return new ImageSurface(pb.Pixels, Format.ARGB32, pb.Width, pb.Height, pb.Rowstride);
         }
 
         public bool Bold
@@ -105,44 +70,41 @@ namespace FamiStudio
 
         protected override bool OnExposeEvent(Gdk.EventExpose ev)
         {
-            base.OnExposeEvent(ev);
-
             var bgColor = Style.Backgrounds[(int)State];
+            var fgColor = Style.Foregrounds[(int)State];
 
             int width  = Allocation.Width;
             int height = Allocation.Height;
 
-            int imgWidth  = (int)(image.Width  / GLTheme.DialogScaling);
-            int imgHeight = (int)(image.Height / GLTheme.DialogScaling);
+            int imgWidth  = (int)(pixbuf.Width  / GLTheme.DialogScaling);
+            int imgHeight = (int)(pixbuf.Height / GLTheme.DialogScaling);
 
-            int x = layoutNormal == null ? (width - imgWidth) / 2 : 5;
+            int x = buttonText == null ? (width - imgWidth) / 2 : 5;
             int y = (height - imgHeight) / 2;
             int yp = State == Gtk.StateType.Active ? 1 : 0;
 
             // Have to use Cairo here since i am unable to draw unpixelated bitmaps with Gdk.
             var ctx = CairoHelper.Create(ev.Window);
-            ctx.SetSourceRGB(bgColor.Red / 65535.0f, bgColor.Green / 65535.0f, bgColor.Blue / 65535.0f);
+
+            CairoHelper.SetSourceColor(ctx, bgColor);
             ctx.Paint();
             ctx.Translate(x, y + yp);
             ctx.Scale(1.0f / GLTheme.DialogScaling, 1.0f / GLTheme.DialogScaling);
-            ctx.SetSource(image);
+            CairoHelper.SetSourcePixbuf(ctx, pixbuf, 0, 0);
             ctx.Paint();
+
+            if (buttonText != null)
+            {
+                CairoHelper.SetSourceColor(ctx, fgColor);
+                ctx.Scale(1.0f, 1.0f);
+                ctx.SelectFontFace("Quicksand", FontSlant.Normal, bold ? FontWeight.Bold : FontWeight.Normal);
+                ctx.SetFontSize(14 * GLTheme.DialogScaling);
+                ctx.MoveTo(20 * GLTheme.DialogScaling, 13 * GLTheme.DialogScaling);
+                ctx.ShowText(buttonText);
+            }
+
             ctx.Target.Dispose();
             ctx.Dispose();
-
-            if (layoutNormal != null)
-            {
-                using (Gdk.GC gc = new Gdk.GC((Drawable)base.GdkWindow))
-                {
-                    var layout = bold ? layoutBold : layoutNormal;
-
-                    layout.Width = Allocation.Width - image.Width - 10;
-                    layout.GetSize(out _, out int layoutHeight);
-                    layoutHeight = Pango.Units.ToPixels(layoutHeight);
-
-                    GdkWindow.DrawLayout(Style.ForegroundGC(State), 10 + image.Width, (height - layoutHeight) / 2 + yp, layout);
-                }
-            }
 
             return true;
         }
