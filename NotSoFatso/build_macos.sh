00851c12dfb3891b07d6dce6e34ace39bcec3a87
BleuBleu <bleubleu@outlook.com>
2020-04-12 00:01:05 +0800

    Compiling NotSoFatso on MacOS.

--- NotSoFatso/build_macos.sh
+++ NotSoFatso/build_macos.sh
@@ -1,2 +1,2 @@
-g++ -dynamiclib -I. -O2 fmopl.c NSF_6502_Trace.cpp Wave_VRC7.cpp NSF.cpp NSF_Core.cpp NSF_6502.cpp NSF_File.cpp -o NotSoFatso.dylib
+g++ -dynamiclib -I. -O2 fmopl.c NSF_Core.cpp NSF_File.cpp NSF_6502.cpp Wave_VRC7.cpp -o NotSoFatso.dylib
 		
