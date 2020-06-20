4370478d48524232732ab349b40fc74e97c2e945
BleuBleu <bleubleu@outlook.com>
2020-06-21 01:44:39 +0800

    Crash fix.

--- FamiStudio/Source/Utils/WinUtils.cs
+++ FamiStudio/Source/Utils/WinUtils.cs
@@ -113,6 +113,9 @@ namespace FamiStudio
                 }
             }
 
+            if (buffer == null)
+                return null;
+
             return ASCIIEncoding.ASCII.GetString(buffer);
         }
 
