de3a66caaad43ff9d6873817d59a4b152192dd0e
BleuBleu <bleubleu@outlook.com>
2020-07-14 21:36:35 +0800

    Move tutorial messages to a class.

--- FamiStudio/UI/Dialogs/WinForms/TutorialDialog.cs
+++ FamiStudio/UI/Dialogs/WinForms/TutorialDialog.cs
@@ -7,28 +7,6 @@ namespace FamiStudio
 {
     public partial class TutorialDialog : Form
     {
-        readonly string[] messages = new[]
-        {
-            @"Welcome to FamiStudio! Let's take a few seconds to review some of the basic controls to make sure you use the app to its fullest.",
-            @"To PAN around the piano roll or the sequencer, simply PRESS and HOLD the MIDDLE MOUSE BUTTON and DRAG around to smoothly move the viewport. Yes, that wheel on your mouse is also a button!",
-            @"To ZOOM in and out in the piano roll or the sequencer, simply rotate the mouse wheel.",
-            @"If you are on a TRACKPAD or a LAPTOP, simply enable TRACKPAD CONTROLS in the settings.",
-            @"To ADD things like patterns and notes, simply CLICK with the LEFT MOUSE BUTTON.",
-            @"To DELETE things like patterns, notes, instruments and songs, simply CLICK on them with the RIGHT MOUSE BUTTON.",
-            @"Always keep and eye on the TOOLTIPS! They change constantly as you move the mouse and they will teach you how to use the app! For the complete DOCUMENTATION and over 1 hour of VIDEO TUTORIAL, please click on the big QUESTION MARK!"
-        };
-
-        readonly string[] images = new[]
-        {
-            "Tutorial0.jpg",
-            "Tutorial1.jpg",
-            "Tutorial2.jpg",
-            "Tutorial3.jpg",
-            "Tutorial4.jpg",
-            "Tutorial5.jpg",
-            "Tutorial6.jpg"
-        };
-
         int pageIndex = 0;
 
         public TutorialDialog()
@@ -74,13 +52,13 @@ namespace FamiStudio
 
         private void SetPage(int idx)
         {
-            pageIndex = Utils.Clamp(idx, 0, messages.Length - 1);
-            pictureBox1.Image = Image.FromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream($"FamiStudio.Resources.{images[pageIndex]}"));
-            label1.Text = messages[pageIndex];
+            pageIndex = Utils.Clamp(idx, 0, TutorialMessages.Messages.Length - 1);
+            pictureBox1.Image = Image.FromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream($"FamiStudio.Resources.{TutorialMessages.Images[pageIndex]}"));
+            label1.Text = TutorialMessages.Messages[pageIndex];
             buttonLeft.Visible = pageIndex != 0;
 
             string suffix = Direct2DTheme.DialogScaling >= 2.0f ? "@2x" : "";
-            buttonRight.Image = pageIndex == messages.Length - 1 ?
+            buttonRight.Image = pageIndex == TutorialMessages.Messages.Length - 1 ?
                 Image.FromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream($"FamiStudio.Resources.Yes{suffix}.png")) :
                 Image.FromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream($"FamiStudio.Resources.ArrowRight{suffix}.png"));
         }
@@ -102,7 +80,7 @@ namespace FamiStudio
 
         private void buttonRight_Click(object sender, EventArgs e)
         {
-            if (pageIndex == messages.Length - 1)
+            if (pageIndex == TutorialMessages.Messages.Length - 1)
             {
                 DialogResult = checkBoxDontShow.Checked ? DialogResult.OK : DialogResult.Cancel;
                 Close();
