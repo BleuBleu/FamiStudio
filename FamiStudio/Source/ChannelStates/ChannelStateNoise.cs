92556a090c9e31fed6b51cd4317c4a583936cece
BleuBleu <bleubleu_@hotmail.com>
2020-08-14 18:25:01 +0800

    SFX export fixes.

--- FamiStudio/Source/ChannelStates/ChannelStateNoise.cs
+++ FamiStudio/Source/ChannelStates/ChannelStateNoise.cs
@@ -19,7 +19,7 @@
                 var period = (int)(((note.Value + envelopeValues[Envelope.Arpeggio]) & 0x0f) ^ 0x0f) | ((duty << 7) & 0x80);
 
                 WriteRegister(NesApu.APU_NOISE_LO, period);
-                WriteRegister(NesApu.APU_NOISE_VOL, 0xf0 | volume);
+                WriteRegister(NesApu.APU_NOISE_VOL, 0x30 | volume);
             }
 
             base.UpdateAPU();
