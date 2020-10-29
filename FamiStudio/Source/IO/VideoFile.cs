9f07936d74f16243c427d8b78a2f3b4ead4dde64
BleuBleu <bleubleu@outlook.com>
2020-10-29 08:08:21 -0700

    Adding hack for OpenGL misbehaving.

--- FamiStudio/Source/IO/VideoFile.cs
+++ FamiStudio/Source/IO/VideoFile.cs
@@ -336,6 +336,40 @@ namespace FamiStudio
             }
         }
 
+#if FAMISTUDIO_LINUX || FAMISTUDIO_MACOS
+        // Some OpenGL implementation applies sRGB to the alpha channel which is super 
+        // wrong. We can detect that assuming the gradient we draw should be at 50% 
+        // opacity in the middle.
+        bool DetectBadOpenGLAlpha(byte[] videoImage)
+        {
+            float midGradientAlpha = videoImage[gradientSizeY / 2 * videoResX * 4 + 3] / 255.0f;
+            float diff = Math.Abs(midGradientAlpha - 0.5f);
+            return diff > 0.05f;
+        }
+
+        // This is really a 1/2.0 gamma curve with a smoother tail. Not real sRGB.
+        static readonly byte[] SRGBToLinear =
+        {
+            //0,16,23,28,32,36,39,42,45,48,50,53,55,58,60,62,64,66,68,70,71,73,75,
+            0,1,2,4,7,10,14,18,23,27,32,38,43,48,52,57,61,65,68,71,73,74,75, // Smooth tail.
+            77,78,80,81,83,84,86,87,89,90,92,93,94,96,97,98,100,101,102,103,105,
+            106,107,108,109,111,112,113,114,115,116,117,118,119,121,122,123,124,
+            125,126,127,128,129,130,131,132,133,134,135,135,136,137,138,139,140,
+            141,142,143,144,145,145,146,147,148,149,150,151,151,152,153,154,155,
+            156,156,157,158,159,160,160,161,162,163,164,164,165,166,167,167,168,
+            169,170,170,171,172,173,173,174,175,176,176,177,178,179,179,180,181,
+            181,182,183,183,184,185,186,186,187,188,188,189,190,190,191,192,192,
+            193,194,194,195,196,196,197,198,198,199,199,200,201,201,202,203,203,
+            204,204,205,206,206,207,208,208,209,209,210,211,211,212,212,213,214,
+            214,215,215,216,217,217,218,218,219,220,220,221,221,222,222,223,224,
+            224,225,225,226,226,227,228,228,229,229,230,230,231,231,232,233,233,
+            234,234,235,235,236,236,237,237,238,238,239,240,240,241,241,242,242,
+            243,243,244,244,245,245,246,246,247,247,248,248,249,249,250,250,251,
+            251,252,252,253,253,254,254,255
+        };
+
+#endif
+
         class VideoChannelState
         {
             public int videoChannelIndex;
@@ -428,6 +462,7 @@ namespace FamiStudio
             if (song.UsesFamiTrackerTempo)
                 SmoothFamiTrackerTempo(metadata);
 
+            var badAlpha     = false;
             var videoImage   = new byte[videoResY * videoResX * 4];
             var channelImage = new byte[channelResY * channelResX * 4];
             var oscilloscope = new float[channelResY, 2]; 
@@ -484,6 +519,13 @@ namespace FamiStudio
                         videoGraphics.EndDraw();
                         videoGraphics.GetBitmap(videoImage);
 
+#if FAMISTUDIO_LINUX || FAMISTUDIO_MACOS
+                        if (f == 0)
+                        {
+                            badAlpha = DetectBadOpenGLAlpha(videoImage);
+                        }
+#endif
+
                         // Render the piano rolls for each channels.
                         foreach (var s in channelStates)
                         {
@@ -529,6 +571,11 @@ namespace FamiStudio
 
                                     if (videoA != 0)
                                     {
+#if FAMISTUDIO_LINUX || FAMISTUDIO_MACOS
+                                        // Fix bad sRGB alpha.
+                                        if (badAlpha)
+                                            videoA = SRGBToLinear[videoA];
+#endif
                                         int videoR = videoImage[videoIdx + 0];
                                         int videoG = videoImage[videoIdx + 1];
                                         int videoB = videoImage[videoIdx + 2];
