3f3f99457fa8980897ff900ba055168c0c28c949
BleuBleu <bleubleu_@hotmail.com>
2021-01-30 13:27:31 +0800

    Small color tweak.

--- FamiStudio/Source/UI/Common/ThemeBase.cs
+++ FamiStudio/Source/UI/Common/ThemeBase.cs
@@ -298,7 +298,11 @@ namespace FamiStudio
 
         public static float ColorDistance(Color c0, Color c1)
         {
-            return (float)Math.Sqrt(Math.Abs(c0.R - c1.R) + Math.Abs(c0.G - c1.G) + Math.Abs(c0.B - c1.B));
+            float dr = Math.Abs(c0.R - c1.R);
+            float dg = Math.Abs(c0.G - c1.G);
+            float db = Math.Abs(c0.B - c1.B);
+
+            return (float)Math.Sqrt(dr * dr + dg * dg + db * db);
         }
 
         public static void EnforceThemeColor(ref Color color)
