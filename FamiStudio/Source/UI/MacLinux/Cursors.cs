6de661e682f516909def94c7d835d9e4601179ad
BleuBleu <bleubleu@outlook.com>
2021-02-04 04:43:12 -0800

    Eyedrop cursor on Linux.

--- FamiStudio/Source/UI/MacLinux/Cursors.cs
+++ FamiStudio/Source/UI/MacLinux/Cursors.cs
@@ -21,7 +21,14 @@ namespace FamiStudio
         public static Gdk.Cursor SizeNS;
         public static Gdk.Cursor DragCursor;
         public static Gdk.Cursor CopyCursor;
+        public static Gdk.Cursor Eyedrop;
 #endif
+
+        private static Gdk.Cursor CreateCursorFromResource(string name, int x, int y)
+        {
+            var pixbuf = Gdk.Pixbuf.LoadFromResource($"FamiStudio.Resources.{name}.png");
+            return new Gdk.Cursor(Gdk.Display.Default, pixbuf, x, y);
+        }
 
         public static void Initialize()
         {
@@ -31,6 +38,7 @@ namespace FamiStudio
             SizeNS     = Gdk.Cursor.NewFromName(Gdk.Display.Default, "row-resize");
             DragCursor = Gdk.Cursor.NewFromName(Gdk.Display.Default, "grab");
             CopyCursor = Gdk.Cursor.NewFromName(Gdk.Display.Default, "copy");
+            Eyedrop    = CreateCursorFromResource("EyedropCursor", 7, 24);
 #endif
         }
     }
