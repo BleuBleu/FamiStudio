9b0e0237397782f827c50372aad8720059dd429d
BleuBleu <bleubleu@outlook.com>
2021-02-06 12:11:49 +0800

    Tweaking package script.

--- Setup/PackageMacOS.sh
+++ Setup/PackageMacOS.sh
@@ -2,6 +2,10 @@
 
 msbuild ../FamiStudio/FamiStudio.Mac.csproj /t:Rebuild /p:Configuration=Release /verbosity:quiet
 
+cp ../FamiStudio/bin/Release/*.exe ./FamiStudio.app/Contents/MacOS/
+cp ../FamiStudio/bin/Release/*.dll ./FamiStudio.app/Contents/MacOS/
+cp ../FamiStudio/bin/Release/*.dylib ./FamiStudio.app/Contents/MacOS/
+
 version=`cat Version.txt`
 filename=FamiStudio$version-MacOS.zip
 
