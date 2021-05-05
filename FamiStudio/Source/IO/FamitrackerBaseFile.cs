fb37fb8987774224ca52a8aa2ae968c96d75ad0b
BleuBleu <bleubleu_@hotmail.com>
2021-05-05 21:56:21 +0800

    Fix inverted slides in VRC7 FTM.

--- FamiStudio/Source/IO/FamitrackerBaseFile.cs
+++ FamiStudio/Source/IO/FamitrackerBaseFile.cs
@@ -596,7 +596,7 @@ namespace FamiStudio
                 var portamentoSpeed = 0;
                 var slideSpeed = 0;
                 var slideShift = c.IsN163WaveChannel ? 2 : 0;
-                var slideSign  = c.IsN163WaveChannel || c.IsFdsWaveChannel ? -1 : 1; // Inverted channels.
+                var slideSign  = c.IsN163WaveChannel || c.IsFdsWaveChannel || c.IsVrc7FmChannel ? -1 : 1; // Inverted channels.
 
                 for (int p = 0; p < s.Length; p++)
                 {
