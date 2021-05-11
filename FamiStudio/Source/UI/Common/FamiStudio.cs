837c6170333ac96e3ab6d20a9592004bb312c048
BleuBleu <bleubleu_@hotmail.com>
2021-05-11 21:54:58 +0800

    No play rate change while playing.

--- FamiStudio/Source/UI/Common/FamiStudio.cs
+++ FamiStudio/Source/UI/Common/FamiStudio.cs
@@ -63,6 +63,7 @@ namespace FamiStudio
         public int BaseRecordingOctave => baseRecordingOctave;
         public int CurrentFrame => lastTickCurrentFrame >= 0 ? lastTickCurrentFrame : (songPlayer != null ? songPlayer.PlayPosition : 0);
         public int ChannelMask { get => songPlayer != null ? songPlayer.ChannelMask : 0xffff; set => songPlayer.ChannelMask = value; }
+        public int PlayRate { get => songPlayer != null ? songPlayer.PlayRate : 1; set { if (!IsPlaying) songPlayer.PlayRate = value; } }
 
         public Project Project => project;
         public Song Song => song;
