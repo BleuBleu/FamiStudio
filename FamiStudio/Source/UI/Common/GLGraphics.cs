e9ee24fa82ccdb1d9fe9b2117d9703a8eb966529
BleuBleu <bleubleu@outlook.com>
2021-08-13 02:41:09 -0700

    Bitmap backgound on Linux/Mac.

--- FamiStudio/Source/UI/Common/GLGraphics.cs
+++ FamiStudio/Source/UI/Common/GLGraphics.cs
@@ -154,9 +154,8 @@ namespace FamiStudio
             return new GLBitmap(CreateTexture(bmp), bmp.Width, bmp.Height);
         }
 
-        private void ChangeBitmapBackground(Bitmap bmp, Color color)
+        private unsafe void ChangeBitmapBackground(Bitmap bmp, Color color)
         {
-            // MATTT : Optimize + port to other platforms.
 #if FAMISTUDIO_WINDOWS
             for (int y = 0; y < bmp.Height; y++)
             {
@@ -170,6 +169,24 @@ namespace FamiStudio
 
                     bmp.SetPixel(x, y, Color.FromArgb(r, g, b));
                 }
+            }
+#else
+            var p = (int*)bmp.Pixels.ToPointer();
+
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    var pixel = Color.FromArgb(p[x]);
+
+                    var r = (byte)Utils.Lerp(color.R, pixel.B, pixel.A / 255.0f);
+                    var g = (byte)Utils.Lerp(color.G, pixel.G, pixel.A / 255.0f);
+                    var b = (byte)Utils.Lerp(color.B, pixel.R, pixel.A / 255.0f);
+
+                    p[x] = Color.FromArgb(b, g, r).ToArgb();
+                }
+
+                p += bmp.Rowstride / 4;
             }
 #endif
         }
