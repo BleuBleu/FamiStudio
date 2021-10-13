0a2b280aecf7e7ca5711ba9097a4b7d864919d57
Perkka2 <36314461+Perkka2@users.noreply.github.com>
2021-10-13 18:28:48 +0200

    updated stereo support on windows

--- FamiStudio/Source/Player/XAudio2Stream.cs
+++ FamiStudio/Source/Player/XAudio2Stream.cs
@@ -33,8 +33,8 @@ namespace FamiStudio
 
         public XAudio2Stream(int rate, int bufferSize, int numBuffers, GetBufferDataCallback bufferFillCallback)
         {
-            //xaudio2 = new XAudio2();
-            xaudio2 = new XAudio2(XAudio2Version.Version27); // To simulate Windows 7 behavior.
+            xaudio2 = new XAudio2();
+            //xaudio2 = new XAudio2(XAudio2Version.Version27); // To simulate Windows 7 behavior.
             //xaudio2.CriticalError += Xaudio2_CriticalError;
             masteringVoice = new MasteringVoice(xaudio2);
             waveFormat = new WaveFormat(rate, 16, 2);
