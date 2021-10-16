d11cc06e2424991c8d003ad6a5a42577bdc86ea2
Perkka2 <36314461+Perkka2@users.noreply.github.com>
2021-10-16 20:37:17 +0200

    Update ChannelStateEPSMRythm.cs

--- FamiStudio/Source/ChannelStates/ChannelStateEPSMRythm.cs
+++ FamiStudio/Source/ChannelStates/ChannelStateEPSMRythm.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace FamiStudio
 {
     public class ChannelStateEPSMRythm : ChannelState
     {
         int channelIdx = 0;
-        int lastNoteOn = 0;
+        int[] opStereo = { 0, 0, 0, 0, 0, 0 };
 
         public ChannelStateEPSMRythm(IPlayerInterface player, int apuIdx, int channelType, bool pal) : base(player, apuIdx, channelType, pal)
         {
@@ -13,27 +14,23 @@ namespace FamiStudio
             customRelease = true;
 
         }
+        protected override void LoadInstrument(Instrument instrument)
+        {
+            Debug.Assert(instrument.ExpansionType == ExpansionType.EPSM);
+            opStereo[channelIdx] = instrument.EpsmPatchRegs[1] & 0xC0;
+        }
 
         public override void UpdateAPU()
         {
-            if (note.IsRelease)
+            if (note.IsMusical)
             {
-                lastNoteOn = 0;
-            }
-            if (note.IsStop)
-            {
-                lastNoteOn = 0;
-            }
-            else if (note.IsMusical)
-            {
-                if (lastNoteOn == 0)
+                if (noteTriggered)
                 {
                     var volume = GetVolume();
-                    lastNoteOn = 1;
                     WriteRegister(NesApu.EPSM_ADDR0, NesApu.EPSM_REG_RYTHM);
                     WriteRegister(NesApu.EPSM_DATA0, (1 << channelIdx));
                     WriteRegister(NesApu.EPSM_ADDR0, NesApu.EPSM_REG_RYTHM_LEVEL + channelIdx);
-                    WriteRegister(NesApu.EPSM_DATA0, 0xc0 | volume << 1);
+                    WriteRegister(NesApu.EPSM_DATA0, opStereo[channelIdx] | volume << 1);
                 }
             }
 
