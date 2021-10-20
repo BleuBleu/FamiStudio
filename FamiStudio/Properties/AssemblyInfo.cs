3c7073908ae86f62116ab525e20a78a7cd9ec835
BleuBleu <bleubleu@outlook.com>
2021-10-20 20:34:19 +0800

    Packaging stuff.

--- FamiStudio/Properties/AssemblyInfo.cs
+++ FamiStudio/Properties/AssemblyInfo.cs
@@ -44,4 +44,11 @@ using Android.App;
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+
+#if DEBUG
+[assembly: Application(Debuggable=true)]
+#else
+[assembly: Application(Debuggable=false)]
+#endif
+
 #endif
