f43380eabe1b50fa8c3d2e50f5574a4c4b002762
BleuBleu <bleubleu_@hotmail.com>
2021-10-21 16:35:28 +0800

    Unit test batch files.

--- FamiStudio/UnitTests/NsfBuildRefs.bat
+++ FamiStudio/UnitTests/NsfBuildRefs.bat
@@ -5,6 +5,7 @@
 ..\bin\Release\FamiStudio.exe TestS5B.fms nsf-export TestS5B.nsf
 ..\bin\Release\FamiStudio.exe TestVRC6.fms nsf-export TestVRC6.nsf
 ..\bin\Release\FamiStudio.exe TestVRC7.fms nsf-export TestVRC7.nsf
+..\bin\Release\FamiStudio.exe TestMulti.fms nsf-export TestMulti.nsf
 ..\bin\Release\FamiStudio.exe TestFamiTrackerTempo.fms nsf-export TestFamiTrackerTempo.nsf
 
 ..\bin\Release\FamiStudio.exe TestBase.nsf famistudio-txt-export TestBase_NsfRef.txt
@@ -14,6 +15,7 @@
 ..\bin\Release\FamiStudio.exe TestS5B.nsf famistudio-txt-export TestS5B_NsfRef.txt
 ..\bin\Release\FamiStudio.exe TestVRC6.nsf famistudio-txt-export TestVRC6_NsfRef.txt
 ..\bin\Release\FamiStudio.exe TestVRC7.nsf famistudio-txt-export TestVRC7_NsfRef.txt
+..\bin\Release\FamiStudio.exe TestMulti.nsf famistudio-txt-export TestMulti_NsfRef.txt
 ..\bin\Release\FamiStudio.exe TestFamiTrackerTempo.nsf famistudio-txt-export TestFamiTrackerTempo_NsfRef.txt
 
 del /q *.nsf
