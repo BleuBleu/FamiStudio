247d35e7cf0fa24373a1663bfc1ae0f5bd70804d
BleuBleu <bleubleu_@hotmail.com>
2023-03-07 17:11:03 +0800

    gcc

--- ThirdParty/Stb/DllWrapper.c
+++ ThirdParty/Stb/DllWrapper.c
@@ -12,17 +12,17 @@
 #include <alloca.h>
 #endif
 
-int StbGetNumberOfFonts(const unsigned char* data)
+int __stdcall StbGetNumberOfFonts(const unsigned char* data)
 {
 	return stbtt_GetNumberOfFonts(data);
 }
 
-int StbGetFontOffsetForIndex(const unsigned char* data, int index)
+int __stdcall StbGetFontOffsetForIndex(const unsigned char* data, int index)
 {
 	return stbtt_GetFontOffsetForIndex(data, index);
 }
 
-void* StbInitFont(const unsigned char* data, int offset)
+void* __stdcall StbInitFont(const unsigned char* data, int offset)
 {
 	stbtt_fontinfo* info = (stbtt_fontinfo*)malloc(sizeof(stbtt_fontinfo));
 
@@ -35,67 +35,67 @@ void* StbInitFont(const unsigned char* data, int offset)
 	return info;
 }
 
-void StbFreeFont(void* font)
+void __stdcall StbFreeFont(void* font)
 {
 	free(font);
 }
 
-float StbScaleForPixelHeight(const stbtt_fontinfo* info, float pixels)
+float __stdcall StbScaleForPixelHeight(const stbtt_fontinfo* info, float pixels)
 {
 	return stbtt_ScaleForPixelHeight(info, pixels);
 }
 
-float StbScaleForMappingEmToPixels(const stbtt_fontinfo* info, float pixels)
+float __stdcall StbScaleForMappingEmToPixels(const stbtt_fontinfo* info, float pixels)
 {
 	return stbtt_ScaleForMappingEmToPixels(info, pixels);
 }
 
-void StbGetGlyphHMetrics(const stbtt_fontinfo* info, int glyph, int* advanceWidth, int* leftSideBearing)
+void __stdcall StbGetGlyphHMetrics(const stbtt_fontinfo* info, int glyph, int* advanceWidth, int* leftSideBearing)
 {
 	stbtt_GetGlyphHMetrics(info, glyph, advanceWidth, leftSideBearing);
 }
 
-int StbGetGlyphKernAdvance(const stbtt_fontinfo* info, int ch1, int ch2)
+int __stdcall StbGetGlyphKernAdvance(const stbtt_fontinfo* info, int ch1, int ch2)
 {
 	return stbtt_GetGlyphKernAdvance(info, ch1, ch2);
 }
 
-void StbGetFontVMetrics(const stbtt_fontinfo* info, int* ascent, int* descent, int* lineGap)
+void __stdcall StbGetFontVMetrics(const stbtt_fontinfo* info, int* ascent, int* descent, int* lineGap)
 {
 	stbtt_GetFontVMetrics(info, ascent, descent, lineGap);
 }
 
-int StbGetFontVMetricsOS2(const stbtt_fontinfo* info, int* typoAscent, int* typoDescent, int* typoLineGap, int* winAscent, int* winDescent)
+int __stdcall StbGetFontVMetricsOS2(const stbtt_fontinfo* info, int* typoAscent, int* typoDescent, int* typoLineGap, int* winAscent, int* winDescent)
 {
 	return stbtt_GetFontVMetricsOS2(info, typoAscent, typoDescent, typoLineGap, winAscent, winDescent);
 }
 
-void StbGetFontBoundingBox(const stbtt_fontinfo* info, int* x0, int* y0, int* x1, int* y1)
+void __stdcall StbGetFontBoundingBox(const stbtt_fontinfo* info, int* x0, int* y0, int* x1, int* y1)
 {
 	stbtt_GetFontBoundingBox(info, x0, y0, x1, y1);
 }
 
-int StbFindGlyphIndex(const stbtt_fontinfo* info, int codepoint)
+int __stdcall StbFindGlyphIndex(const stbtt_fontinfo* info, int codepoint)
 {
 	return stbtt_FindGlyphIndex(info, codepoint);
 }
 
-void StbGetGlyphBitmapBox(const stbtt_fontinfo* info, int glyph, float scale, int* x0, int* y0, int* x1, int* y1)
+void __stdcall StbGetGlyphBitmapBox(const stbtt_fontinfo* info, int glyph, float scale, int* x0, int* y0, int* x1, int* y1)
 {
 	stbtt_GetGlyphBitmapBox(info, glyph, scale, scale, x0, y0, x1, y1);
 }
 
-void StbMakeGlyphBitmap(const stbtt_fontinfo* info, unsigned char* output, int width, int height, int stride, int glyph, float scale)
+void __stdcall StbMakeGlyphBitmap(const stbtt_fontinfo* info, unsigned char* output, int width, int height, int stride, int glyph, float scale)
 {
 	stbtt_MakeGlyphBitmap(info, output, width, height, stride, scale, scale, glyph);
 }
 
-void StbMakeGlyphBitmapSubpixel(const stbtt_fontinfo* info, unsigned char* output, int width, int height, int stride, int glyph, float scale, float subx, float suby)
+void __stdcall StbMakeGlyphBitmapSubpixel(const stbtt_fontinfo* info, unsigned char* output, int width, int height, int stride, int glyph, float scale, float subx, float suby)
 {
 	stbtt_MakeGlyphBitmapSubpixel(info, output, width, height, stride, scale, scale, subx, suby, glyph);
 }
 
-void* StbInitPackRect(int width, int height, int numNodes)
+void* __stdcall StbInitPackRect(int width, int height, int numNodes)
 {
 	stbrp_context* context = (stbrp_context*)malloc(sizeof(stbrp_context));
 	stbrp_node* nodes = (stbrp_node*)malloc(sizeof(stbrp_node) * numNodes);
@@ -104,13 +104,13 @@ void* StbInitPackRect(int width, int height, int numNodes)
 	return context;
 }
 
-void StbFreePackRect(stbrp_context* pack)
+void __stdcall StbFreePackRect(stbrp_context* pack)
 {
 	free(pack->orig_nodes);
 	free(pack);
 }
 
-int StbPackRects(stbrp_context* pack, const int* widths, const int* heights, int* x, int* y, int num)
+int __stdcall StbPackRects(stbrp_context* pack, const int* widths, const int* heights, int* x, int* y, int num)
 {
 	// FONTTODO: Im on a plane, no documentation. Figure out which header has alloca.
 #if defined(_MSC_VER)
