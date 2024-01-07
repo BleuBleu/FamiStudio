2b46af658f5e712f5f17120ad9d5a44db1f25c2c
BleuBleu <bleubleu_@hotmail.com>
2024-01-07 19:16:01 +0800

    Fix PrintCodeSize.

--- FamiStudio/Nsf/PrintCodeSize.cs
+++ FamiStudio/Nsf/PrintCodeSize.cs
@@ -7,33 +7,27 @@ namespace PrintCodeSize
     {
         static void Main(string[] args)
         {
-            try
+            var bytes = File.ReadAllBytes(args[0]);
+            var bankCount = int.Parse(args[1].Substring(12)); // {CODEBANKS}=X
+
+            var driverSize = bytes.Length;
+
+            for (; driverSize >= 1; driverSize--)
             {
-                var lines = File.ReadAllLines(args[0]);
-                var codeSize = 0;
-                var songDataStart = 0;
-
-                foreach (var line in lines)
-                {
-                    if (line.StartsWith("CODE "))
-                    {
-                        var splits = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                        codeSize = Convert.ToInt32(splits[3], 16);
-                    }
-                    else if (line.StartsWith("SONG_DATA "))
-                    {
-                        var splits = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                        songDataStart = Convert.ToInt32(splits[1], 16);
-                    }
-                }
-
-                var roundedCodeSize = ((codeSize + 127) & ~127);
-                var allocatedCodeSize = (songDataStart - 0x8080);
-                if ((roundedCodeSize % 256) == 0)
-                roundedCodeSize += 128;
-                Console.WriteLine($"    Code size is {codeSize.ToString("X")} (Padded size = {roundedCodeSize.ToString("X")}, Allocated size = {allocatedCodeSize.ToString("X")} {(roundedCodeSize != allocatedCodeSize ? "*** RESIZE NEEDED ***" : "")})");
+                if (bytes[driverSize - 1] != 0)
+                    break;
             }
-            catch {}
+
+            driverSize -= 128; // NSF header.
+            driverSize = ((driverSize + 4095) / 4096) * 4096;
+
+            var driverBankCount = driverSize / 4096;
+
+            if (driverBankCount != bankCount)
+            {
+                Console.WriteLine($"*** BANK COUNT MISMATCH! Banks needed = {driverBankCount}, Banks specified = {bankCount}");            
+            }
         }
     }
 }
+
