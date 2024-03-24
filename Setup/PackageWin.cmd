350bbf30da64845c3aa5db97b1da2773927387c0
BleuBleu <bleubleu_@hotmail.com>
2024-03-24 09:15:50 +0800

    Setup update for 4.2.0 (and VS2022).

--- Setup/PackageWin.cmd
+++ Setup/PackageWin.cmd
@@ -1,5 +1,5 @@
 if not defined DevEnvDir (
-	@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
+	@call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 )	
 
 :: Not building those since visual studio builds arent deterministic (they contain timestamps). 
@@ -11,14 +11,14 @@ REM msbuild ..\ThirdParty\ShineMp3\ShineMp3.vcxproj /t:Rebuild /p:Configuration=
 msbuild ..\FamiStudio\FamiStudio.csproj /t:Rebuild /p:Configuration=Release /p:Platform="AnyCPU" /p:SolutionDir="%~dp0../" /verbosity:quiet
 
 :: This is needed to build setup project, well know visual studio issue.
-cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\VSI\DisableOutOfProcBuild"
+cd "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\VSI\DisableOutOfProcBuild\"
 @call DisableOutOfProcBuild.exe
 
 :: Build setup.
 cd "%~dp0"
 devenv ..\FamiStudio.sln /Project Setup /rebuild Release
 
-cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\VSI\DisableOutOfProcBuild"
+cd "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\VSI\DisableOutOfProcBuild\"
 @call DisableOutOfProcBuild.exe undo
 
 :: Zip and create 3 versions (zipped installer, installer, portable exe)
@@ -30,5 +30,5 @@ cd Release
 tar -a -c -f ..\FamiStudio%Version%-WinInstaller.zip Setup.msi Setup.exe
 cd ..
 
-del ..\FamiStudio\bin\Release\net5.0\FamiStudio.Android.dll
-tar -a -c -f FamiStudio%Version%-WinPortableExe.zip "Demo Songs\*.*" "Demo Instruments\*.*" WindowsDotNetReadme.txt portable.txt -C ..\FamiStudio Localization\*.ini -C bin\Release\net5.0 *.exe *.dll *.json FamiStudio.pdb
+del ..\FamiStudio\bin\Release\net7.0\FamiStudio.Android.dll
+tar -a -c -f FamiStudio%Version%-WinPortableExe.zip "Demo Songs\*.*" "Demo Instruments\*.*" WindowsDotNetReadme.txt portable.txt -C ..\FamiStudio Localization\*.ini -C bin\Release\net7.0 *.exe *.dll *.json FamiStudio.pdb
