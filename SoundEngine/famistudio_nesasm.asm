7dc5ae817bbb11163649ccd3d946db63fce1e012
James Rowe <jroweboy@gmail.com>
2024-04-05 20:45:39 -0400

    Reapply "Merge pull request #298 from jroweboy/rainbow"
    This reverts commit 9aa3a68ea75d7e640fede6e0a748a3262e9c1dec.

--- SoundEngine/famistudio_nesasm.asm
+++ SoundEngine/famistudio_nesasm.asm
@@ -114,7 +114,10 @@ FAMISTUDIO_NESASM_CODE_ORG     = $8000
 ;======================================================================================================================
 
 ; Konami VRC6 (2 extra square + saw)
-; FAMISTUDIO_EXP_VRC6          = 1 
+; FAMISTUDIO_EXP_VRC6          = 1
+
+; Rainbow-Net (homebrew clone of VRC6)
+; FAMISTUDIO_EXP_RAINBOW       = 1
 
 ; Konami VRC7 (6 FM channels)
 ; FAMISTUDIO_EXP_VRC7          = 1 
@@ -267,6 +270,14 @@ FAMISTUDIO_DPCM_OFF = $c000
 ;======================================================================================================================
 ; INTERNAL DEFINES (Do not touch)
 ;======================================================================================================================
+
+    .ifndef FAMISTUDIO_EXP_RAINBOW
+FAMISTUDIO_EXP_RAINBOW = 0
+    .endif
+
+    .if FAMISTUDIO_EXP_RAINBOW
+FAMISTUDIO_EXP_VRC6 = 1
+    .endif
 
     .ifndef FAMISTUDIO_EXP_VRC6
 FAMISTUDIO_EXP_VRC6 = 0
@@ -1066,6 +1077,17 @@ FAMISTUDIO_APU_DMC_LEN    = $4013
 FAMISTUDIO_APU_SND_CHN    = $4015
 FAMISTUDIO_APU_FRAME_CNT  = $4017
 
+    .if FAMISTUDIO_EXP_RAINBOW
+FAMISTUDIO_VRC6_PL1_VOL   = $41a0
+FAMISTUDIO_VRC6_PL1_LO    = $41a1
+FAMISTUDIO_VRC6_PL1_HI    = $41a2
+FAMISTUDIO_VRC6_PL2_VOL   = $41a3
+FAMISTUDIO_VRC6_PL2_LO    = $41a4
+FAMISTUDIO_VRC6_PL2_HI    = $41a5
+FAMISTUDIO_VRC6_SAW_VOL   = $41a6
+FAMISTUDIO_VRC6_SAW_LO    = $41a7
+FAMISTUDIO_VRC6_SAW_HI    = $41a8
+    .else
 FAMISTUDIO_VRC6_PL1_VOL   = $9000
 FAMISTUDIO_VRC6_PL1_LO    = $9001
 FAMISTUDIO_VRC6_PL1_HI    = $9002
@@ -1075,6 +1097,7 @@ FAMISTUDIO_VRC6_PL2_HI    = $a002
 FAMISTUDIO_VRC6_SAW_VOL   = $b000
 FAMISTUDIO_VRC6_SAW_LO    = $b001
 FAMISTUDIO_VRC6_SAW_HI    = $b002
+    .endif
 
 FAMISTUDIO_VRC7_SILENCE   = $e000
 FAMISTUDIO_VRC7_REG_SEL   = $9010
