a619c2a645844cd5e435e00a32ede6cd725ecfc7
BleuBleu <bleubleu_@hotmail.com>
2024-04-13 16:12:53 +0800

    Beta 3.

--- FamiStudio/Properties/AssemblyInfo.cs
+++ FamiStudio/Properties/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using Android.App;
 
 // The last digit is the BETA version number. When it is non-zero, the build and will be 
 // interpreted as a development version.
-[assembly: AssemblyVersion("4.2.0.2")]
-[assembly: AssemblyFileVersion("4.2.0.2")]
+[assembly: AssemblyVersion("4.2.0.3")]
+[assembly: AssemblyFileVersion("4.2.0.3")]
 
 #if FAMISTUDIO_ANDROID
 // Add some common permissions, these can be removed if not needed
