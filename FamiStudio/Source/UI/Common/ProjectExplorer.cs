35f6974e8236677822894095d02feecae19f2eff
BleuBleu <bleubleu_@hotmail.com>
2024-05-18 17:49:12 +0800

    Remove useless "add" DPCM sample button.

--- FamiStudio/Source/UI/Common/ProjectExplorer.cs
+++ FamiStudio/Source/UI/Common/ProjectExplorer.cs
@@ -503,8 +503,8 @@ namespace FamiStudio
                     }
                     case ButtonType.DpcmHeader:
                     {
-                        var buttons = new[] { SubButtonType.Add, SubButtonType.Load, SubButtonType.Sort };
-                        active = projectExplorer.App.Project.AutoSortSamples ? -1 : 3;
+                        var buttons = new[] { SubButtonType.Load, SubButtonType.Sort };
+                        active = projectExplorer.App.Project.AutoSortSamples ? -1 : 1;
                         return buttons;
                     }
                     case ButtonType.ProjectSettings:
