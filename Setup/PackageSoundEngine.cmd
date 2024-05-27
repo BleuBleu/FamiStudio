1b290edf554f53cc2cd0c4c73dccd32c4af7fc25
BleuBleu <bleubleu_@hotmail.com>
2024-05-27 15:32:33 +0800

    Including .inc file in demo.

--- Setup/PackageSoundEngine.cmd
+++ Setup/PackageSoundEngine.cmd
@@ -1,7 +1,7 @@
 set /p Version=<Version.txt
 if not exist ..\FamiStudio%Version%-SoundEngine mkdir ..\FamiStudio%Version%-SoundEngine
 robocopy ..\SoundEngine\ ..\FamiStudio%Version%-SoundEngine *.h *.asm *.nes *.s *.txt
-robocopy ..\SoundEngine\DemoSource\ ..\FamiStudio%Version%-SoundEngine\DemoSource *.c *.asm *.s *.cfg *.chr *.dmc *.pal *.rle build*.bat export_from_famistudio.bat
+robocopy ..\SoundEngine\DemoSource\ ..\FamiStudio%Version%-SoundEngine\DemoSource *.c *.asm *.s *.cfg *.chr *.dmc *.pal *.rle build*.bat demo_ca65.inc export_from_famistudio.bat
 del ..\FamiStudio%Version%-SoundEngine\DemoSource\*_cc65.s
 del ..\FamiStudio%Version%-SoundEngine\*multi*.s
 tar -a -c -f FamiStudio%Version%-SoundEngine.zip -C ..\FamiStudio%Version%-SoundEngine\ *
