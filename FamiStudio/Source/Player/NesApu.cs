fdce96230b68ebb8124d8618b86e3c9a45b4a75f
Steo <acidreflux64@protonmail.com>
2024-11-10 06:30:12 +0000

    Removed pointless namespaces in NesApu.cs

--- FamiStudio/Source/Player/NesApu.cs
+++ FamiStudio/Source/Player/NesApu.cs
@@ -1,14 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reflection.PortableExecutable;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
-using Microsoft.VisualBasic;
 
 namespace FamiStudio
 {
