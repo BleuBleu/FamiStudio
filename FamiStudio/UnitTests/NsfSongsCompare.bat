4ebc206545e554ecea2e664f0882daa19d74299a
BleuBleu <bleubleu_@hotmail.com>
2024-11-23 14:25:31 +0800

    Fix net8.0 path everywhere.

--- FamiStudio/UnitTests/NsfSongsCompare.bat
+++ FamiStudio/UnitTests/NsfSongsCompare.bat
@@ -3,17 +3,17 @@
 "C:\Program Files (x86)\FamiStudio\FamiStudio.exe" "C:\Program Files (x86)\FamiStudio\Demo Songs\Tower of Heaven.fms" nsf-export D:\Dump\Tower331.nsf
 "C:\Program Files (x86)\FamiStudio\FamiStudio.exe" "C:\Program Files (x86)\FamiStudio\Demo Songs\Lagrange Point.fms" nsf-export D:\Dump\Lagrange331.nsf
 
-..\bin\Release\net7.0\FamiStudio.exe D:\Dump\Another331.nsf famistudio-txt-export D:\Dump\Another331.txt -nsf-import-pattern-length:120 -famistudio-txt-bare
-..\bin\Release\net7.0\FamiStudio.exe D:\Dump\Gimmick331.nsf famistudio-txt-export D:\Dump\Gimmick331.txt -nsf-import-pattern-length:120 -famistudio-txt-bare
-..\bin\Release\net7.0\FamiStudio.exe D:\Dump\Tower331.nsf famistudio-txt-export D:\Dump\Tower331.txt -nsf-import-pattern-length:120 -famistudio-txt-bare
-..\bin\Release\net7.0\FamiStudio.exe D:\Dump\Lagrange331.nsf famistudio-txt-export D:\Dump\Lagrange331.txt -nsf-import-pattern-length:120 -famistudio-txt-bare
+..\bin\Release\net8.0\FamiStudio.exe D:\Dump\Another331.nsf famistudio-txt-export D:\Dump\Another331.txt -nsf-import-pattern-length:120 -famistudio-txt-bare
+..\bin\Release\net8.0\FamiStudio.exe D:\Dump\Gimmick331.nsf famistudio-txt-export D:\Dump\Gimmick331.txt -nsf-import-pattern-length:120 -famistudio-txt-bare
+..\bin\Release\net8.0\FamiStudio.exe D:\Dump\Tower331.nsf famistudio-txt-export D:\Dump\Tower331.txt -nsf-import-pattern-length:120 -famistudio-txt-bare
+..\bin\Release\net8.0\FamiStudio.exe D:\Dump\Lagrange331.nsf famistudio-txt-export D:\Dump\Lagrange331.txt -nsf-import-pattern-length:120 -famistudio-txt-bare
 
-..\bin\Release\net7.0\FamiStudio.exe "C:\Program Files (x86)\FamiStudio\Demo Songs\Another Winter.fms" nsf-export D:\Dump\Another400.nsf
-..\bin\Release\net7.0\FamiStudio.exe "C:\Program Files (x86)\FamiStudio\Demo Songs\Gimmick.fms" nsf-export D:\Dump\Gimmick400.nsf
-..\bin\Release\net7.0\FamiStudio.exe "C:\Program Files (x86)\FamiStudio\Demo Songs\Tower of Heaven.fms" nsf-export D:\Dump\Tower400.nsf
-..\bin\Release\net7.0\FamiStudio.exe "C:\Program Files (x86)\FamiStudio\Demo Songs\Lagrange Point.fms" nsf-export D:\Dump\Lagrange400.nsf
+..\bin\Release\net8.0\FamiStudio.exe "C:\Program Files (x86)\FamiStudio\Demo Songs\Another Winter.fms" nsf-export D:\Dump\Another400.nsf
+..\bin\Release\net8.0\FamiStudio.exe "C:\Program Files (x86)\FamiStudio\Demo Songs\Gimmick.fms" nsf-export D:\Dump\Gimmick400.nsf
+..\bin\Release\net8.0\FamiStudio.exe "C:\Program Files (x86)\FamiStudio\Demo Songs\Tower of Heaven.fms" nsf-export D:\Dump\Tower400.nsf
+..\bin\Release\net8.0\FamiStudio.exe "C:\Program Files (x86)\FamiStudio\Demo Songs\Lagrange Point.fms" nsf-export D:\Dump\Lagrange400.nsf
 
-..\bin\Release\net7.0\FamiStudio.exe D:\Dump\Another400.nsf famistudio-txt-export D:\Dump\Another400.txt -nsf-import-pattern-length:120 -famistudio-txt-bare
-..\bin\Release\net7.0\FamiStudio.exe D:\Dump\Gimmick400.nsf famistudio-txt-export D:\Dump\Gimmick400.txt -nsf-import-pattern-length:120 -famistudio-txt-bare
-..\bin\Release\net7.0\FamiStudio.exe D:\Dump\Tower400.nsf famistudio-txt-export D:\Dump\Tower400.txt -nsf-import-pattern-length:120 -famistudio-txt-bare
-..\bin\Release\net7.0\FamiStudio.exe D:\Dump\Lagrange400.nsf famistudio-txt-export D:\Dump\Lagrange400.txt -nsf-import-pattern-length:120 -famistudio-txt-bare
+..\bin\Release\net8.0\FamiStudio.exe D:\Dump\Another400.nsf famistudio-txt-export D:\Dump\Another400.txt -nsf-import-pattern-length:120 -famistudio-txt-bare
+..\bin\Release\net8.0\FamiStudio.exe D:\Dump\Gimmick400.nsf famistudio-txt-export D:\Dump\Gimmick400.txt -nsf-import-pattern-length:120 -famistudio-txt-bare
+..\bin\Release\net8.0\FamiStudio.exe D:\Dump\Tower400.nsf famistudio-txt-export D:\Dump\Tower400.txt -nsf-import-pattern-length:120 -famistudio-txt-bare
+..\bin\Release\net8.0\FamiStudio.exe D:\Dump\Lagrange400.nsf famistudio-txt-export D:\Dump\Lagrange400.txt -nsf-import-pattern-length:120 -famistudio-txt-bare
